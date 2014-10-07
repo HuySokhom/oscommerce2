7ddb31445b55afba40ccb50840c26516be05da75
john <kom.huy@gmail.com>
2014-10-07 14:22:37 +0700

    remove install folder

--- catalog/includes/languages/english/modules/shipping/usps.php
+++ catalog/includes/languages/english/modules/shipping/usps.php
@@ -1,19 +1,31 @@
 <?php
 /*
-  $Id: usps.php,v 1.8 2003/02/14 12:54:37 dgw_ Exp $
+$Id: usps.php 1739 2007-12-20 00:52:16Z hpdl $
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
+osCommerce, Open Source E-Commerce Solutions
+http://www.oscommerce.com
 
-  Copyright (c) 2002 osCommerce
+Copyright - 2002 osCommerce
 
-  Released under the GNU General Public License
+Released under the GNU General Public License
 */
 
-define('MODULE_SHIPPING_USPS_TEXT_TITLE', 'United States Postal Service');
-define('MODULE_SHIPPING_USPS_TEXT_DESCRIPTION', 'United States Postal Service<br><br>You will need to have registered an account with USPS at http://www.uspsprioritymail.com/et_regcert.html to use this module<br><br>USPS expects you to use pounds as weight measure for your products.');
-define('MODULE_SHIPPING_USPS_TEXT_OPT_PP', 'Parcel Post');
-define('MODULE_SHIPPING_USPS_TEXT_OPT_PM', 'Priority Mail');
-define('MODULE_SHIPPING_USPS_TEXT_OPT_EX', 'Express Mail');
-define('MODULE_SHIPPING_USPS_TEXT_ERROR', 'An error occured with the USPS shipping calculations.<br>If you prefer to use USPS as your shipping method, please contact the store owner.');
-?>
+define('MODULE_SHIPPING_USPS_TEXT_TITLE', 'United States Postal Service&nbsp;<br>');
+define('MODULE_SHIPPING_USPS_TEXT_DESCRIPTION', 'United States Postal Service<br><br>You will need to have registered an account with USPS at https://secure.shippingapis.com/registration/ to use this module<br><br>USPS expects you to use pounds as weight measure for your products.');
+define('MODULE_SHIPPING_USPS_TEXT_ERROR', 'An error occured with the USPS shipping calculations.<br>If you would like to use USPS as your shipping method, please contact the store owner.');
+define('MODULE_SHIPPING_USPS_TEXT_DAY', 'Day');
+define('MODULE_SHIPPING_USPS_TEXT_DAYS', 'Business Days');
+define('MODULE_SHIPPING_USPS_TEXT_WEEKS', 'Weeks');
+define('MODULE_SHIPPING_USPS_TEXT_BUSINESS_DAYS', 'Business Days');
+define('MODULE_SHIPPING_USPS_TEXT_VARIES', 'varies by destination');
+define('MODULE_SHIPPING_USPS_TEXT_ESTIMATED', '---Approx. ');
+define('MODULE_SHIPPING_USPS_TEXT_INSURED', '---Insured for ');
+
+define('MODULE_SHIPPING_USPS_TEXT_ESTIMATED_FROM_DATE_OF_SHIPMENT', ' from date of shipment');
+
+define('MODULE_SHIPPING_USPS_TEXT_WEIGHT_DISPLAY', ' (%d pkg, %01.2f lbs total)');
+define('MODULE_SHIPPING_USPS_TEXT_CONNECTION_ERROR', 'Failed to connect to the USPS Web Tools&trade; postage rates server.');
+define('MODULE_SHIPPING_USPS_ERROR_NO_SERVICES', 'No allowed USPS services could be used to ship this package.<br />If you would like to use USPS as your shipping method, please contact the store owner.');
+
+
+
