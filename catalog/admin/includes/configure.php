31e1ffa48466bda99ce4903ca07ea641c291e414
john <kom.huy@gmail.com>
2014-10-08 11:57:20 +0700

    configure file

--- catalog/admin/includes/configure.php
+++ catalog/admin/includes/configure.php
@@ -1,27 +1,14 @@
 <?php
-/*
-  $Id: configure.php,v 1.14 2003/02/21 16:55:24 dgw_ Exp $
-
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2002 osCommerce
-
-  Released under the GNU General Public License
-*/
-
-// define our webserver variables
-// FS = Filesystem (physical)
-// WS = Webserver (virtual)
-  define('HTTP_SERVER', ''); // eg, http://localhost or - https://localhost should not be NULL for productive servers
-  define('HTTP_CATALOG_SERVER', '');
-  define('HTTPS_CATALOG_SERVER', '');
-  define('ENABLE_SSL_CATALOG', 'false'); // secure webserver for catalog module
-  define('DIR_FS_DOCUMENT_ROOT', $DOCUMENT_ROOT); // where your pages are located on the server. if $DOCUMENT_ROOT doesnt suit you, replace with your local path. (eg, /usr/local/apache/htdocs)
-  define('DIR_WS_ADMIN', '/admin/');
-  define('DIR_FS_ADMIN', DIR_FS_DOCUMENT_ROOT . DIR_WS_ADMIN);
-  define('DIR_WS_CATALOG', '/catalog/');
-  define('DIR_FS_CATALOG', DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG);
+error_reporting(E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED ^ E_NOTICE);
+  define('HTTP_SERVER', 'http://localhost');
+  define('HTTP_CATALOG_SERVER', 'http://localhost');
+  define('HTTPS_CATALOG_SERVER', 'http://localhost');
+  define('ENABLE_SSL_CATALOG', 'false');
+  define('DIR_FS_DOCUMENT_ROOT', '/var/www/OSC2.2USPS7.3/catalog/');
+  define('DIR_WS_ADMIN', '/OSC2.2USPS7.3/catalog/admin/');
+  define('DIR_FS_ADMIN', '/var/www/OSC2.2USPS7.3/catalog/admin/');
+  define('DIR_WS_CATALOG', '/OSC2.2USPS7.3/catalog/');
+  define('DIR_FS_CATALOG', '/var/www/OSC2.2USPS7.3/catalog/');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
@@ -37,11 +24,10 @@
   define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
   define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
 
-// define our database connection
-  define('DB_SERVER', '');
-  define('DB_SERVER_USERNAME', 'mysql');
-  define('DB_SERVER_PASSWORD', '');
-  define('DB_DATABASE', 'osCommerce');
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', 'root');
+  define('DB_DATABASE', 'osc_usps');
   define('USE_PCONNECT', 'false');
-  define('STORE_SESSIONS', '');
+  define('STORE_SESSIONS', 'mysql');
 ?>
--- catalog/includes/configure.php
+++ catalog/includes/configure.php
@@ -1,27 +1,14 @@
 <?php
-/*
-  $Id: configure.php,v 1.14 2003/07/09 01:15:48 hpdl Exp $
-
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2003 osCommerce
-
-  Released under the GNU General Public License
-*/
-
-// Define the webserver and path parameters
-// * DIR_FS_* = Filesystem directories (local/physical)
-// * DIR_WS_* = Webserver directories (virtual/URL)
-  define('HTTP_SERVER', ''); // eg, http://localhost - should not be empty for productive servers
-  define('HTTPS_SERVER', ''); // eg, https://localhost - should not be empty for productive servers
-  define('ENABLE_SSL', false); // secure webserver for checkout procedure?
-  define('HTTP_COOKIE_DOMAIN', '');
-  define('HTTPS_COOKIE_DOMAIN', '');
-  define('HTTP_COOKIE_PATH', '');
-  define('HTTPS_COOKIE_PATH', '');
-  define('DIR_WS_HTTP_CATALOG', '');
-  define('DIR_WS_HTTPS_CATALOG', '');
+error_reporting(E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED ^ E_NOTICE);
+  define('HTTP_SERVER', 'http://localhost');
+  define('HTTPS_SERVER', 'http://localhost');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', 'localhost');
+  define('HTTPS_COOKIE_DOMAIN', 'localhost');
+  define('HTTP_COOKIE_PATH', '/OSC2.2USPS7.3/catalog/');
+  define('HTTPS_COOKIE_PATH', '/OSC2.2USPS7.3/catalog/');
+  define('DIR_WS_HTTP_CATALOG', '/OSC2.2USPS7.3/catalog/');
+  define('DIR_WS_HTTPS_CATALOG', '/OSC2.2USPS7.3/catalog/');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_INCLUDES', 'includes/');
@@ -32,15 +19,14 @@
   define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
 
   define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
-  define('DIR_FS_CATALOG', dirname($HTTP_SERVER_VARS['SCRIPT_FILENAME']) . '/');
+  define('DIR_FS_CATALOG', '/var/www/OSC2.2USPS7.3/catalog/');
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
 
-// define our database connection
-  define('DB_SERVER', ''); // eg, localhost - should not be empty for productive servers
-  define('DB_SERVER_USERNAME', '');
-  define('DB_SERVER_PASSWORD', '');
-  define('DB_DATABASE', 'osCommerce');
-  define('USE_PCONNECT', 'false'); // use persistent connections?
-  define('STORE_SESSIONS', ''); // leave empty '' for default handler or set to 'mysql'
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', 'root');
+  define('DB_DATABASE', 'osc_usps');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
 ?>
